45a9e61c027ab003a819adb028f016aa6e7a6575
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-06 02:17:24 +0530

    First Commit

---
+++ js/music-list.js
@@ -0,0 +1,21 @@
+let allMusic = [
+  {
+    name: "Tujh Mein Rab Dikhta Hai",
+    artist: "Roopkumar Rathod",
+    img: "7",
+    src: "7",
+  },
+
+  {
+    name: "Saiyaara",
+    artist: "Mohit Chauhan",
+    img: "10",
+    src: "10",
+  },
+  {
+    name: "Deva-Deva (Brahmastra)",
+    artist: "Arijit Singh, Jonita Gandhi",
+    img: "1",
+    src: "1",
+  },
+];
